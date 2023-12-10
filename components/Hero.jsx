aadb9100d4e7f36d77d5ed1319386510a570cc22
The Hieu <thehieu236@gmail.com>
2023-12-10 23:37:36 +0700

    Update hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,19 @@
+"use client";
+
 import { Download, Send } from "lucide-react";
 import Link from "next/link";
-import { RiArrowDownSLine } from "react-icons/ri";
+import {
+  RiArrowDownSLine,
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+} from "react-icons/ri";
 import { Button } from "./ui/button";
 
 import DevImg from "./DevImg";
 import Badge from "./Badge";
 import Socials from "./Socials";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -34,10 +42,46 @@ const Hero = () => {
                 <Download size={18} />
               </Button>
             </div>
+
+            {/* socials */}
+            <Socials
+              containerStyles={"flex gap-x-6 mx-auto xl:mx-0"}
+              iconsStyles={
+                "text-foreground text-[22px] hover:text-primary transition-all"
+              }
+            />
           </div>
 
           {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            {/* badge */}
+            <Badge
+              containerStyles={"absolute top-[24%] -left-[5rem]"}
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText={"Year Of Experience"}
+            />
+            {/* badge 2*/}
+            <Badge
+              containerStyles={"absolute top-[80%] -left-[1rem]"}
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              badgeText={"Fineshed Prjects"}
+            />
+            {/* badge 3*/}
+            <Badge
+              containerStyles={"absolute top-[55%] -right-8"}
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText={"k"}
+              badgeText={"Happy Clients"}
+            />
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/dev.png"
+            />
+          </div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-1/2 bottom-44 xl:bottom-12 animate-bounce">
