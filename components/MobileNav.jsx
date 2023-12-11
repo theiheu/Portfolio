006c239e9ad52e7ce3b6eb5cd27ffd9c89cdf922
The Hieu <thehieu236@gmail.com>
2023-12-11 10:36:09 +0700

    Add mobile navigation component

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,5 +1,26 @@
+import Logo from "@/app/Logo";
+import Nav from "@/app/Nav";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+import Socials from "./Socials";
+
 const MobileNav = () => {
-  return <div></div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col justify-between items-center h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav containerStyles={"flex flex-col items-center gap-y-6"} />
+            <Socials containerStyles={"flex gap-x-4"} />
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
